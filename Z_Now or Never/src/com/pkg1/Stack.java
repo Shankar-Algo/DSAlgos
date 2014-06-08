dd8f3fdb9faf0c42d580a13bfbe8595c8a2ca697
Shankar-Algo <shankar.ssithamb@gmail.com>
2014-06-08 13:11:18 +0530

    Adding Stack

---
+++ Z_Now or Never/src/com/pkg1/Stack.java
@@ -0,0 +1,41 @@
+package com.pkg1;
+
+public class Stack {
+
+	public static void main(String[] args) 
+	{
+	
+		Stack obj = new Stack();
+		obj.init();
+	
+	}
+	void init()
+	{
+		this.push(10);
+		this.push(20);
+		this.push(30);
+		System.out.println((int)this.pop());
+	}
+	
+	 Node top; 
+	  Object pop() { 
+		  if (top != null) 
+		  { 
+			  Object item = top.d; 
+			  top = top.next; 
+			  
+			  return item;   
+		  }     
+		  return null; 
+		  } 
+	  void push(int item) 
+	  { 
+		  Node t = new Node(item); 
+		 
+		  t.next = top;
+		  
+		  top = t;
+		 
+	  } 
+
+}
